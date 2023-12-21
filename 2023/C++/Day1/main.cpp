c562d3c99e0671484d8cc40d07efb7830833c293
raedaddala <addala.raed@gmail.com>
2023-12-21 19:31:27 +0100

    2023 Day 1 Part 2 in C++

--- 2023/C++/Day1/main.cpp
+++ 2023/C++/Day1/main.cpp
@@ -15,7 +15,7 @@ int main(void) {
             lines.push_back(line);
         }
         std::cout << "Part 1 :" << part1(lines) << std::endl;
-        //std::cout << "Part 2 :" << part2(lines) << std::endl;
+        std::cout << "Part 2 :" << part2(lines) << std::endl;
     }
     return(0);
 }
@@ -32,12 +32,34 @@ unsigned long long part1(std::vector<std::string>& text)
                 if (!num) num = curr;
             }
         }
-        res += num*10+curr;
+        res += num * 10 + curr;
     }
     return res;
 }
 unsigned long long part2(std::vector<std::string>& text)
 {
     static const std::unordered_map<std::string, int> words = { {"one", 1}, {"two", 2}, {"six", 6},{"four", 4}, {"five", 5}, {"nine", 9},{"three", 3}, {"seven", 7}, {"eight", 8}, };
-
+    unsigned long long res = 0;
+    for (auto s : text) {
+        int num = 0;
+        int curr = 0;
+        for (int i = 0; s[i] != '\0';++i) {
+            if (std::isdigit(s[i])) {
+                curr = s[i] - '0';
+                if (!num) num = curr;
+            }
+            else {
+                for (auto& key : words) {
+                    int size = key.first.size();
+                    std::string sub = s.substr(i, size);
+                    if (sub == key.first) {
+                        curr = key.second;
+                        if (!num) num = curr;
+                    }
+                }
+            }
+        }
+        res += num * 10 + curr;
+    }
+    return res;
 }
